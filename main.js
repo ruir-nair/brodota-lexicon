bbe45619e224a25b0b988566d6b11e803f54bbf0
ArxNair <36222269+ruir-nair@users.noreply.github.com>
2023-12-06 02:09:53 +0700

    Buff

--- main.js
+++ main.js
@@ -16,52 +16,62 @@ document.addEventListener('DOMContentLoaded', function () {
             fetch('data/tier.json')
                 .then(response => response.json())
                 .then(tierColors => {
-                    // Function to filter the table based on search input
-                    const filterTable = () => {
-                        const searchTerm = searchBox.value.toLowerCase();
+                    // Fetch buffs from 'data/buff.json'
+                    fetch('data/buff.json')
+                        .then(response => response.json())
+                        .then(buffs => {
+                            // Function to filter the table based on search input
+                            const filterTable = () => {
+                                const searchTerm = searchBox.value.toLowerCase();
 
-                        const filteredData = originalData.filter(item =>
-                            item.name.toLowerCase().includes(searchTerm) ||
-                            item.tier.toLowerCase().includes(searchTerm) ||
-                            item.effect.toLowerCase().includes(searchTerm)
-                        );
+                                const filteredData = originalData.filter(item =>
+                                    item.name.toLowerCase().includes(searchTerm) ||
+                                    item.tier.toLowerCase().includes(searchTerm) ||
+                                    item.effect.toLowerCase().includes(searchTerm)
+                                );
 
-                        // Clear the current table
-                        itemTableBody.innerHTML = '';
+                                // Clear the current table
+                                itemTableBody.innerHTML = '';
 
-                        // Display each filtered item in the table
-                        filteredData.forEach(item => {
-                            const row = document.createElement('tr');
+                                // Display each filtered item in the table
+                                filteredData.forEach(item => {
+                                    const row = document.createElement('tr');
 
-                            const nameCell = document.createElement('td');
-                            nameCell.textContent = item.name;
-                            row.appendChild(nameCell);
+                                    const nameCell = document.createElement('td');
+                                    nameCell.textContent = item.name;
+                                    row.appendChild(nameCell);
 
-                            const tierCell = document.createElement('td');
-                            tierCell.textContent = item.tier;
-                            row.appendChild(tierCell);
+                                    const tierCell = document.createElement('td');
+                                    tierCell.textContent = item.tier;
+                                    row.appendChild(tierCell);
 
-                            const effectCell = document.createElement('td');
-                            effectCell.textContent = item.effect;
-                            row.appendChild(effectCell);
+                                    const effectCell = document.createElement('td');
+                                    effectCell.textContent = item.effect;
+                                    row.appendChild(effectCell);
 
-                            // Set background color based on tier
-                            const tierColor = tierColors.find(tier => tier.tier === item.tier)?.color || 'white';
-                            row.style.backgroundColor = tierColor;
+                                    // Display buffs in one column separated by commas
+                                    const buffsCell = document.createElement('td');
+                                    buffsCell.textContent = item.buffs.map(buff => {
+                                        const buffInfo = buffs.find(b => b._id === buff._id);
+                                        return `${buffInfo.name}: ${buff.value}`;
+                                    }).join(', ');
+                                    row.appendChild(buffsCell);
 
-                            const tierColorCell = document.createElement('td');
-                            tierColorCell.textContent = tierColor;
-                            row.appendChild(tierColorCell);
+                                    // Set background color based on tier
+                                    const tierColor = tierColors.find(tier => tier.tier === item.tier)?.color || 'white';
+                                    row.style.backgroundColor = tierColor;
 
-                            itemTableBody.appendChild(row);
-                        });
-                    };
+                                    itemTableBody.appendChild(row);
+                                });
+                            };
 
-                    // Add event listener for the search box
-                    searchBox.addEventListener('input', filterTable);
+                            // Add event listener for the search box
+                            searchBox.addEventListener('input', filterTable);
 
-                    // Initial display
-                    filterTable();
+                            // Initial display
+                            filterTable();
+                        })
+                        .catch(error => console.error('Error fetching buffs:', error));
                 })
                 .catch(error => console.error('Error fetching tier colors:', error));
         })
