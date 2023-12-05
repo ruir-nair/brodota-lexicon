81f3e4455c1df46177ac943e2a46bf98f957acb4
ArxNair <36222269+ruir-nair@users.noreply.github.com>
2023-12-06 01:58:03 +0700

    JSON concept of DB

--- main.js
+++ main.js
@@ -1,31 +1,41 @@
 document.addEventListener('DOMContentLoaded', function () {
     const itemTableBody = document.getElementById('itemTableBody');
 
-    // Fetch items from item.json
-    fetch('item.json')
+    // Fetch items from 'data/item.json'
+    fetch('data/item.json')
         .then(response => response.json())
         .then(data => {
             // Clear the current table
             itemTableBody.innerHTML = '';
 
-            // Display each item in the table
-            data.forEach(item => {
-                const row = document.createElement('tr');
+            // Fetch tier colors from 'data/tier.json'
+            fetch('data/tier.json')
+                .then(response => response.json())
+                .then(tierColors => {
+                    // Display each item in the table
+                    data.forEach(item => {
+                        const row = document.createElement('tr');
 
-                const nameCell = document.createElement('td');
-                nameCell.textContent = item.name;
-                row.appendChild(nameCell);
+                        const nameCell = document.createElement('td');
+                        nameCell.textContent = item.name;
+                        row.appendChild(nameCell);
 
-                const tierCell = document.createElement('td');
-                tierCell.textContent = item.tier;
-                row.appendChild(tierCell);
+                        const tierCell = document.createElement('td');
+                        tierCell.textContent = item.tier;
+                        row.appendChild(tierCell);
 
-                const effectCell = document.createElement('td');
-                effectCell.textContent = item.effect;
-                row.appendChild(effectCell);
+                        const effectCell = document.createElement('td');
+                        effectCell.textContent = item.effect;
+                        row.appendChild(effectCell);
 
-                itemTableBody.appendChild(row);
-            });
+                        // Set background color based on tier
+                        const tierColor = tierColors[item.tier] || 'white';
+                        row.style.backgroundColor = tierColor;
+
+                        itemTableBody.appendChild(row);
+                    });
+                })
+                .catch(error => console.error('Error fetching tier colors:', error));
         })
-        .catch(error => console.error('Error:', error));
+        .catch(error => console.error('Error fetching items:', error));
 });
